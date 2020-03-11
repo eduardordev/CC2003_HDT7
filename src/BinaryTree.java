62f5414849875e5e27d303cd6b6ca5fae0ab2081
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2020-03-11 10:55:22 -0600

    nuevas clases creadas

---
+++ src/BinaryTree.java
@@ -0,0 +1,14 @@
+public class BinaryTree{
+
+	Node root;
+
+	private Node addRecursive(Node current, String word){
+		if(current == null){
+			return new Node(word);
+		}
+
+		if(word.compareTo(current.word)){
+			
+		}
+	}
+}
---
+++ src/Node.java
@@ -0,0 +1,17 @@
+/*
+*@author: Eduardo Ramírez #19946
+*Algoritmos y estructuras de datos
+*HDT7
+*/
+public class Node{
+
+	String word;
+	Node left;
+	Node right;
+
+	Node(String word){
+		this.word = word;
+		left = null;
+		right = null;
+	}
+}
